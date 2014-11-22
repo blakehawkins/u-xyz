5609b6f96fb9dd06a10a46850c55ed37989e70f7
Blake Hawkins <b.hawkins@sms.ed.ac.uk>
2014-11-22 18:55:19 +0000

    transition to live data

--- app/statistics/statistics.js
+++ app/statistics/statistics.js
@@ -9,174 +9,155 @@ angular.module('myApp.statistics', ['ngRoute'])
   });
 }])
 
-.controller('StatisticsCtrl', [function() {
-    var margins = {
-    top: 12,
-    left: 80,
-    right: 0,
-    bottom: 24
-},
-legendPanel = {
-    width: 180
-},
-width = 800 - margins.left - margins.right,
-    height = 100 - margins.top - margins.bottom,
-    dataset = [{
-        data: [{
-            month: 'Performance',
-            count: 20
-        }],
-        name: 'a'
-    }, {
-        data: [{
-            month: 'Performance',
-            count: 30
-        }],
-        name: 'b'
-    }, {
-        data: [{
-            month: 'Performance',
-            count: 10
-        }],
-        name: 'c'
-    }, {
-        data: [{
-            month: 'Performance',
-            count: 400
-        }],
-        name: 'd'
-    }
-    , {
-        data: [{
-            month: 'Performance',
-            count: 340
-        }],
-        name: 'd'
-    }
+.controller('StatisticsCtrl', function($scope, $http) {
+    // request the hardcoded stats data
+    $http.get('http://localhost:27080/test/data/_find')
+            .success(function(data){
+                $scope.dataset = data.results;
+                draw_graph();
+            });
 
-    ],
-    series = dataset.map(function (d) {
-        return d.name;
-    }),
-    dataset = dataset.map(function (d) {
-        return d.data.map(function (o, i) {
-            // Structure it so that your numeric
-            // axis (the stacked amount) is y
-            return {
-                y: o.count,
-                x: o.month
-            };
-        });
-    }),
-    stack = d3.layout.stack();
-
-stack(dataset);
-
-var dataset = dataset.map(function (group) {
-    return group.map(function (d) {
-        // Invert the x and y values, and y0 becomes x0
-        return {
-            x: d.y,
-            y: d.x,
-            x0: d.y0
+    var draw_graph = function(){
+        var margins = {
+            top: 12,
+            left: 80,
+            right: 0,
+            bottom: 24
         };
-    });
-}),
-    svg = d3.select('#performance')
-        .append('svg')
-        .attr('width', width + margins.left + margins.right + legendPanel.width)
-        .attr('height', height + margins.top + margins.bottom)
-        .append('g')
-        .attr('transform', 'translate(' + margins.left + ',' + margins.top + ')'),
-    xMax = d3.max(dataset, function (group) {
-        return d3.max(group, function (d) {
-            return d.x + d.x0;
+
+        var width = 800 - margins.left - margins.right,
+            height = 100 - margins.top - margins.bottom,
+            series = $scope.dataset.map(function (d) {
+                return d.name;
+            });
+
+        $scope.dataset = $scope.dataset.map(function(o, i){
+            // console.log(o);
+            // return [o.category, o.count];
+            return [{y: o.count, x: o.category, y0: 0}];
         });
-    }),
-    xScale = d3.scale.linear()
-            .domain([0, 800])
-            .range([0, width]),
-    months = dataset[0].map(function (d) {
-        return d.y;
-    }),
-    _ = console.log(months),
-    yScale = d3.scale.ordinal()
-        .domain(months)
-        .rangeRoundBands([0, height], .1),
-    yAxis = d3.svg.axis()
-        .scale(yScale)
-        .tickSize(0)
-        // .tickValues([])
-        // .attr('width', '1')
-        // .classed('axis-line', true)
-        .orient('left'),
-    yAxis2 = d3.svg.axis()
-        .scale(yScale)
-        .tickSize(0)
-        .tickValues("")
-        // .attr('width', '1')
-        // .classed('axis-line', true)
-        .orient('right'),
-    colours = d3.scale.category10(),
-    groups = svg.selectAll('g')
-        .data(dataset)
-        .enter()
-        .append('g')
-        .style('fill', function (d, i) {
-        return colours(i);
-    }),
-    rects = groups.selectAll('rect')
-        .data(function (d) {
-        return d;
-    })
-        .enter()
-        .append('rect')
-        .attr('stroke', 'black')
-        .attr('stroke-width', '3')
-        // .attr('fill', 'white')
-        .attr('x', function (d) {
-        return xScale(d.x0);
-    })
-        .attr('y', function (d, i) {
-        return yScale(d.y);
-    })
-        .attr('height', function (d) {
-        return yScale.rangeBand();
-    })
-        .attr('width', function (d) {
-        return xScale(d.x);
-    })
-        .on('mouseover', function (d) {
-        var xPos = parseFloat(d3.select(this).attr('x')) / 2 + width / 2;
-        var yPos = parseFloat(d3.select(this).attr('y')) + yScale.rangeBand() / 2;
+        console.log($scope.dataset);
 
-        d3.select('#tooltip')
-            .style('left', xPos + 'px')
-            .style('top', yPos + 'px')
-            .select('#value')
-            .text(d.x);
+        var stack = d3.layout.stack();
 
-        d3.select('#tooltip').classed('hidden', false);
-    })
-        .on('mouseout', function () {
-        d3.select('#tooltip').classed('hidden', true);
-    })
+        stack($scope.dataset);
 
-    svg.append('g')
+        $scope.dataset = $scope.dataset.map(function (group) {
+            return group.map(function (d) {
+                // Invert the x and y values, and y0 becomes x0
+                return {
+                    x: d.y,
+                    y: d.x,
+                    x0: d.y0
+                };
+            });
+        });
+
+        var svg = d3.select('#performance')
+            .append('svg')
+            .attr('width', width + margins.left + margins.right)
+            .attr('height', height + margins.top + margins.bottom)
+            .append('g')
+            .attr('transform', 'translate(' + margins.left + ',' + margins.top + ')');
+
+        var xMax = d3.max($scope.dataset, function (group) {
+            return d3.max(group, function (d) {
+                return d.x + d.x0;
+            });
+        });
+
+        var xScale = d3.scale.linear()
+                .domain([0, 800])
+                .range([0, width]);
+
+        var categorys = $scope.dataset[0].map(function (d) {
+            return d.y;
+        });
+
+
+        var yScale = d3.scale.ordinal()
+            .domain(categorys)
+            .rangeRoundBands([0, height], .1);
+
+        var yAxis = d3.svg.axis()
+            .scale(yScale)
+            .tickSize(0)
+            // .tickValues([])
+            // .attr('width', '1')
+            // .classed('axis-line', true)
+            .orient('left');
+
+        var yAxis2 = d3.svg.axis()
+            .scale(yScale)
+            .tickSize(0)
+            .tickValues("")
+            // .attr('width', '1')
+            // .classed('axis-line', true)
+            .orient('right');
+
+        var colours = d3.scale.category10();
+
+        var groups = svg.selectAll('g')
+            .data($scope.dataset)
+            .enter()
+            .append('g')
+            .style('fill', function (d, i) {
+            return colours(i);
+        });
+
+        var rects = groups.selectAll('rect')
+            .data(function (d) {
+                return d;
+            })
+            .enter()
+            .append('rect')
+            .attr('stroke', 'black')
+            .attr('stroke-width', '3')
+            // .attr('fill', 'white')
+            .attr('x', function (d) {
+                return xScale(d.x0);
+            })
+            .attr('y', function (d, i) {
+                return yScale(d.y);
+            })
+            .attr('height', function (d) {
+                return yScale.rangeBand();
+            })
+            .attr('width', function (d) {
+                return xScale(d.x);
+            })
+            .on('mouseover', function (d) {
+                var xPos = parseFloat(d3.select(this).attr('x')) / 2 + width / 2;
+                var yPos = parseFloat(d3.select(this).attr('y')) + yScale.rangeBand() / 2;
+
+                d3.select('#tooltip')
+                    .style('left', xPos + 'px')
+                    .style('top', yPos + 'px')
+                    .select('#value')
+                    .text(d.x);
+
+                d3.select('#tooltip').classed('hidden', false);
+            })
+            .on('mouseout', function () {
+                d3.select('#tooltip').classed('hidden', true);
+            });
+
+        svg.append('g')
+                .attr('class', 'axis')
+                .attr('transform', 'translate(0,' + height + ')');
+
+        svg.append('g')
+                .attr('class', 'axis')
+                .attr('transform', 'translate(' + margins.left + ', ' + margins.top + ')');
+
+        svg.append('g')
             .attr('class', 'axis')
-            .attr('transform', 'translate(0,' + height + ')');
+            .attr('transform', 'translate(-3, 0)')
+            .call(yAxis);
 
-    svg.append('g')
+        svg.append('g')
             .attr('class', 'axis')
-            .attr('transform', 'translate(' + margins.left + ', ' + margins.top + ')');
-
-svg.append('g')
-    .attr('class', 'axis')
-    .attr('transform', 'translate(-3, 0)')
-    .call(yAxis);
-
-svg.append('g')
-    .attr('class', 'axis')
-    .attr('transform', 'translate(724, 0)')
-    .call(yAxis2);
-}]);
+            .attr('transform', 'translate(724, 0)')
+            .call(yAxis2);
+    }
+});
