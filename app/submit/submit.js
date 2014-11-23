0fc436cd8d9c585748a8f05369cd88f1730d99e1
Pavel Mrlik <hello@maxifick.com>
2014-11-23 16:45:54 +0000

    Form data loading in controller

--- app/submit/submit.js
+++ app/submit/submit.js
@@ -10,10 +10,37 @@ angular.module('myApp.submit', ['ngRoute'])
         });
 }])
 
-.controller('SubmitCtrl', [
+.controller('SubmitCtrl', function ($scope, $http) {
 
-    function () {
+		var pushThatStuffSomewhere = function(data) {
 
-        //$(document).foundation();
+	        $http.post('http://54.77.53.126:27080/test/data/_store', data)
+	        	.success(function(data, status, headers, config) {
+	        	    console.log("That went well.");
+	        	})
+				.error(function(data, status, headers, config) {
+					console.log("Ain't workin'. Have a Redbull and fix it.");
+				});
+		}
 
-}]);
+		$scope.submitClicked = function() {
+			var data = {
+				nationality: $scope.nationality,
+				age: $scope.age,
+				gender: $scope.gender,
+				unistatus: $scope.unistatus, 
+				entry: $scope.entryyear,
+				uni: $scope.university,
+				program: $scope.program,
+				pastgrad: $scope.pastgrad,
+				previousInstitute: $scope.prev,
+				scholarship: $scope.sch,
+				experience: $scope.exp,
+				degree: $scope.degree,
+				grade: $scope.selectedGrade,
+				range: $scope.selectedRange
+			}
+			console.log(data);
+		}
+
+});
